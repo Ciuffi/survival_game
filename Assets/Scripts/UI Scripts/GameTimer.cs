218b00ed8a7c186dafc24f89c43ceff82152ad66
Jack <yiming7391@gmail.com>
2023-02-25 21:53:38 -0800

    tuning + timer

---
+++ Assets/Scripts/UI Scripts/GameTimer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+
+    public TextMeshProUGUI timerText; // Reference to the UI Text component to display the timer
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float timeElapsed = Time.timeSinceLevelLoad;
+        UpdateTimerUI(timeElapsed);
+    }
+
+    void UpdateTimerUI(float timeElapsed)
+    {
+        int minutes = Mathf.FloorToInt(timeElapsed / 60);
+        int seconds = Mathf.FloorToInt(timeElapsed % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
