0819915dcebe7b4c4c947ad4a1400211bb020224
jack <yiming7391@gmail.com>
2023-07-08 15:32:22 -0700

    fix build path

--- Assets/Editor/post-build-push-ipa-appstore.bash
+++ Assets/Editor/post-build-push-ipa-appstore.bash
@@ -3,7 +3,7 @@
 echo "Uploading IPA to Appstore Connect..."
  
 #Path is "/BUILD_PATH/<ORG_ID>.<PROJECT_ID>.<BUILD_TARGET_ID>/.build/last/<BUILD_TARGET_ID>/build.ipa"
-path="$WORKSPACE/.build/last/production-ios/build.ipa"
+path="$WORKSPACE/.build/last/default-ios/build.ipa"
  
 if xcrun altool --upload-app -f $path -u $ITUNES_USERNAME -p $ITUNES_PASSWORD --type ios ; then
     echo "Upload IPA to Appstore Connect finished with success"
